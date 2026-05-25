5e07dd601c6fb57d36177095c71269dcaf831ca9
MAGGZS <magdielbr1@gmail.com>
2026-05-24 21:00:31 -0300

    teste

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
           transition={{ duration: 0.8, delay: 0.25 }}
         >
           Assembleia de Deus<br />
-          <em>Paulistana <span className="hero-badge">Paulistana</span></em>
+          <em className='hero-badge'>Paulistana</em>
         </motion.h1>
 
         <motion.p
